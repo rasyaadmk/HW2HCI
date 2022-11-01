d2913a83d9c7f936cca595ed072518455947c13a
Rasyaad Maulana Khandiyas <91830379+rasyaadmk@users.noreply.github.com>
2022-11-02 02:02:31 +0700

    Initial commit

---
+++ src/views/HomePage.vue
@@ -0,0 +1,240 @@
+<template>
+  <ion-page>
+    <ion-grid>
+      <h1>Home</h1>
+      <ion-row>
+        <ion-col>
+          <ion-card>
+            <ion-card-header>
+              <ion-card-title>Kolam</ion-card-title>
+              <ion-card-subtitle>10</ion-card-subtitle>
+            </ion-card-header>
+
+
+          </ion-card>
+        </ion-col>
+        <ion-col>
+          <ion-card>
+            <ion-card-header>
+              <ion-card-title>Kolam Aktif</ion-card-title>
+              <ion-card-subtitle>8</ion-card-subtitle>
+            </ion-card-header>
+
+
+          </ion-card>
+        </ion-col>
+      </ion-row>
+      <ion-row>
+        <ion-col>
+          <ion-card>
+            <ion-card-header>
+              <ion-card-title>Ikan Hidup</ion-card-title>
+              <ion-card-subtitle>182 Ekor</ion-card-subtitle>
+            </ion-card-header>
+
+
+          </ion-card>
+        </ion-col>
+        <ion-col>
+          <ion-card>
+            <ion-card-header>
+              <ion-card-title>Ikan Mati</ion-card-title>
+              <ion-card-subtitle>89 Ekor</ion-card-subtitle>
+            </ion-card-header>
+
+
+          </ion-card>
+        </ion-col>
+      </ion-row>
+
+      <ion-row>
+        <ion-col>
+          <ion-card>
+            <ion-card-header>
+              <ion-card-title>Panen 2022</ion-card-title>
+              <ion-card-subtitle>109 Kg</ion-card-subtitle>
+            </ion-card-header>
+
+
+          </ion-card>
+        </ion-col>
+        <ion-col>
+          <ion-card>
+            <ion-card-header>
+              <ion-card-title>Total Pakan</ion-card-title>
+              <ion-card-subtitle>223 Kg</ion-card-subtitle>
+            </ion-card-header>
+          </ion-card>
+        </ion-col>
+      </ion-row>
+
+      <h1>Total Berat Ikan</h1>
+
+      <ion-row>
+
+        <h1>
+          <ion-card>
+            <center>
+              <img alt="Lele"
+                src="https://w7.pngwing.com/pngs/77/215/png-transparent-wels-catfish-oily-fish-carp-hazard-graphe-animals-photography-fauna-thumbnail.png" />
+            </center>
+            <ion-card-header>
+              <head-title>Lele</head-title>
+              <ion-card-title>
+                <blue>200kg</blue>
+              </ion-card-title>
+            </ion-card-header>
+          </ion-card>
+        </h1>
+
+        <h1>
+          <ion-card>
+            <center>
+              <img alt="Nila Merah" src="https://efishery.com/wp-content/uploads/2022/05/ikan-nila-nifi.png" />
+            </center>
+
+            <ion-card-header>
+              <head-title>Nila Merah</head-title>
+              <ion-card-title>
+                <blue>200kg</blue>
+              </ion-card-title>
+            </ion-card-header>
+          </ion-card>
+        </h1>
+      </ion-row>
+
+
+      <h1>Kualitas Air</h1>
+
+      <ion-row>
+
+        <h1>
+          <ion-card>
+            <ion-card-header>
+              <h2>pH</h2>
+              <br>
+              <subtitle>Normal <blue>6 Kolam</blue>
+              </subtitle><br>
+              <subtitle>Abnormal <blue>6 Kolam</blue>
+              </subtitle>
+
+            </ion-card-header>
+          </ion-card>
+        </h1>
+
+        <h1>
+          <ion-card>
+            <ion-card-header>
+              <h2>DO</h2>
+              <br>
+              <subtitle>Normal <blue> 6 Kolam</blue>
+              </subtitle><br>
+              <subtitle>Abnormal <blue> 6 Kolam</blue>
+              </subtitle>
+
+            </ion-card-header>
+          </ion-card>
+        </h1>
+
+
+      </ion-row>
+
+    </ion-grid>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import { IonCol, IonGrid, IonRow } from '@ionic/vue';
+import { IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonPage } from '@ionic/vue';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'HomePage',
+  components: { IonCol, IonGrid, IonRow, IonCard, IonCardHeader, IonPage, IonCardSubtitle, IonCardTitle, },
+  setup() {
+    const slideOpts = {
+      initialSlide: 2,
+      speed: 400
+    };
+    return { slideOpts }
+  }
+});
+</script>
+
+<style scoped>
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  background-color: white;
+}
+
+h1 {
+  font-size: 20px;
+  font-weight: 600;
+  margin-left: 10px;
+}
+
+ion-card-title {
+  font-size: 20px;
+  color: black;
+  font-weight: 700;
+}
+
+ion-card-subtitle {
+  font-size: 25px;
+  font-weight: 600;
+
+  /* margin-top: 10px; */
+  text-transform: capitalize;
+  color: black;
+}
+
+subtitle {
+  font-size: 15px;
+  font-weight: 600;
+
+  /* margin-top: 10px; */
+  text-transform: capitalize;
+  color: black;
+}
+
+ion-card {
+  background-color: aliceblue;
+  /* height: 100px; */
+  padding: 0;
+  margin: 2px;
+
+}
+
+img {
+  margin: auto;
+  padding: 10px;
+  height: 150px;
+  width: 180px;
+}
+
+h2 {
+  font-size: 30px;
+  color: black;
+  margin: 0;
+  font-weight: 600;
+}
+
+blue {
+  color: #2C96F1;
+  font-weight: bold;
+}
+
+head-title {
+  margin-top: 0;
+  font-size: 20px;
+  color: black;
+}
+
+
+/* ion-col {
+      background-color: #135d54;
+      border: solid 1px #fff;
+      color: #fff;
+      text-align: center;
+    } */
+</style>
---
+++ src/views/Page2.vue
@@ -0,0 +1,249 @@
+<template>
+  <ion-page>
+    <ion-header :translucent="true">
+      <ion-toolbar>
+        <ion-title>Kolam</ion-title>
+      </ion-toolbar>
+    </ion-header>
+
+    <ion-content :fullscreen="true">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">Kolam</ion-title>
+        </ion-toolbar>
+      </ion-header>
+      <div>
+        <ion-card class="box">
+          <ion-card-header>
+            <div class="cardhead" style="display: flex; justify-content: space-between;">
+              <ion-card-title style="color: black; margin-top: 12px;">Alpha</ion-card-title>
+              <button class="actv">Aktif</button>
+            </div>
+            <hr>
+          </ion-card-header>
+
+          <ion-card-content>
+            <div class="hari" style="display: flex; gap: 10px; align-items: center;">
+              <img src="/assets/page2/Calendar Plus.svg">
+              <p>01-09-2022</p>
+            </div>
+            <div class="hari" style="display: flex; gap: 10px; align-items: center;">
+              <img src="/assets/page2/Timesheet.svg">
+              <p>90 Hari</p>
+            </div>
+            <div class="hari" style="display: flex; gap: 10px; align-items: center;">
+              <img src="/assets/page2/Whole Fish.svg">
+              <p>200 Ekor</p>
+            </div>
+          </ion-card-content>
+        </ion-card>
+
+        <ion-card class="box">
+          <ion-card-header>
+            <div class="cardhead" style="display: flex; justify-content: space-between;">
+              <ion-card-title style="color: black; margin-top: 12px;">Beta</ion-card-title>
+              <button class="actv">Aktif</button>
+            </div>
+            <hr>
+          </ion-card-header>
+
+          <ion-card-content>
+            <div class="hari" style="display: flex; gap: 10px; align-items: center;">
+              <img src="/assets/page2/Calendar Plus.svg">
+              <p>01-09-2022</p>
+            </div>
+            <div class="hari" style="display: flex; gap: 10px; align-items: center;">
+              <img src="/assets/page2/Timesheet.svg">
+              <p>90 Hari</p>
+            </div>
+            <div class="hari" style="display: flex; gap: 10px; align-items: center;">
+              <img src="/assets/page2/Whole Fish.svg">
+              <p>200 Ekor</p>
+            </div>
+          </ion-card-content>
+        </ion-card>
+        <ion-card class="box">
+          <ion-card-header>
+            <div class="cardhead" style="display: flex; justify-content: space-between;">
+              <ion-card-title style="color: black; margin-top: 12px;">Charlie</ion-card-title>
+              <button class="actv">Aktif</button>
+            </div>
+            <hr>
+          </ion-card-header>
+
+          <ion-card-content>
+            <div class="hari" style="display: flex; gap: 10px; align-items: center;">
+              <img src="/assets/page2/Calendar Plus.svg">
+              <p>01-09-2022</p>
+            </div>
+            <div class="hari" style="display: flex; gap: 10px; align-items: center;">
+              <img src="/assets/page2/Timesheet.svg">
+              <p>90 Hari</p>
+            </div>
+            <div class="hari" style="display: flex; gap: 10px; align-items: center;">
+              <img src="/assets/page2/Whole Fish.svg">
+              <p>200 Ekor</p>
+            </div>
+          </ion-card-content>
+        </ion-card>
+        <ion-card class="box">
+          <ion-card-header>
+            <div class="cardhead" style="display: flex; justify-content: space-between;">
+              <ion-card-title style="color: black; margin-top: 12px;">Delta</ion-card-title>
+              <button class="non-active">Tidak Aktif</button>
+            </div>
+            <hr>
+          </ion-card-header>
+
+          <ion-card-content>
+            <div class="hari" style="display: flex; gap: 10px; align-items: center;">
+              <img src="/assets/page2/Calendar Plus.svg">
+              <p>01-09-2022</p>
+            </div>
+            <div class="hari" style="display: flex; gap: 10px; align-items: center;">
+              <img src="/assets/page2/Timesheet.svg">
+              <p>90 Hari</p>
+            </div>
+            <div class="hari" style="display: flex; gap: 10px; align-items: center;">
+              <img src="/assets/page2/Whole Fish.svg">
+              <p>200 Ekor</p>
+            </div>
+          </ion-card-content>
+        </ion-card>
+        <ion-card class="box">
+          <ion-card-header>
+            <div class="cardhead" style="display: flex; justify-content: space-between;">
+              <ion-card-title style="color: black; margin-top: 12px;">Epsilon</ion-card-title>
+              <button class="actv">Aktif</button>
+            </div>
+            <hr>
+          </ion-card-header>
+
+          <ion-card-content>
+            <div class="hari" style="display: flex; gap: 10px; align-items: center;">
+              <img src="/assets/page2/Calendar Plus.svg">
+              <p>01-09-2022</p>
+            </div>
+            <div class="hari" style="display: flex; gap: 10px; align-items: center;">
+              <img src="/assets/page2/Timesheet.svg">
+              <p>90 Hari</p>
+            </div>
+            <div class="hari" style="display: flex; gap: 10px; align-items: center;">
+              <img src="/assets/page2/Whole Fish.svg">
+              <p>200 Ekor</p>
+            </div>
+          </ion-card-content>
+        </ion-card>
+      </div>
+    </ion-content>
+  </ion-page>
+
+</template>
+  
+<script lang="ts">
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonIcon } from '@ionic/vue';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'PageSecond',
+  components: {
+    IonContent,
+    IonHeader,
+    IonPage,
+    IonTitle,
+    IonToolbar
+  }
+});
+</script>
+  
+<style scoped>
+@import 
+* {
+  font-family: Poppins;
+}
+hr {
+  border-top: 2px solid black;
+  margin-bottom: -10px;
+}
+
+@font-face {
+  font-family: "Poppins";
+  src: url(../../public/assets/fonts/Poppins-Regular.ttf);
+}
+
+.non-active {
+  margin-top: 10px;
+  margin-left: 180px;
+  justify-content: center;
+  align-items: center;
+  padding: 5px 10px;
+  gap: 10px;
+
+  width: 100px;
+  height: 34px;
+
+  background: #c20000;
+  border-radius: 12px;
+}
+
+#container {
+  text-align: center;
+
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 50%;
+  transform: translateY(-50%);
+}
+
+#container strong {
+  font-size: 20px;
+  line-height: 26px;
+}
+
+#container p {
+  font-size: 16px;
+  line-height: 22px;
+
+  color: #8c8c8c;
+
+  margin: 0;
+}
+
+#container a {
+  text-decoration: none;
+}
+
+.box {
+  padding: 1px 0px;
+  font-family: poppin;
+  background-color: white;
+  color: black;
+  margin: 30px;
+  border-radius: 16px;
+  font-size: 18px;
+  font-weight: 600;
+  line-height: 27px;
+}
+
+.cardhead {
+  columns: 2;
+  margin-block: -5px, 10px;
+}
+
+.actv {
+  margin-top: 10px;
+  margin-left: 180px;
+  justify-content: center;
+  align-items: center;
+  padding: 5px 10px;
+  gap: 10px;
+
+  width: 58px;
+  height: 34px;
+
+  background: #1B7C1A;
+  border-radius: 12px;
+}
+</style>
+  
